0ccbb8bda5364b44cc0bda8a8ab4436dda3b08b1
Hillary Muller <hillaryaward@gmail.com>
2023-03-20 16:59:14 -0500

    commit tree bcc4fb0bedff6f2577aee6a639b391749af3331e

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,4 +2,27 @@ class Tree
   def initialize(root = nil)
     @root = root
   end
+
+  def breadth_first_traversal(node)
+    result = []
+    nodes_to_visit = [node]
+
+    while nodes_to_visit.length > 0
+      node = nodes_to_visit.shift
+      result.push(node[:value])
+      nodes_to_visit = node[:children] + node_to_visit
+    end
+  end
+  def get_element_by_id(str)
+   nodes_to_visit = [@root]
+
+   until nodes_to_visit.empty?
+    current = nodes_to_visit.pop
+    return current if current[:id] == str
+    nodes_to_visit = nodes_to_visit + current[:children]
+   end
+   nil
+    
+  end
+  
 end
